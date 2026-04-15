51700b56d51da7d91c6081471a5ea1220bad2235
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-15 20:46:28 +0500

    feat: add initial setup

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 
+input("> Enter your 'query' for model response or type 'exist' to leave the sesstion: ")
+while True:
+    query = input(">")
+
+    if query == "exist":
+        print("Ending sesstion..")
+        break
+
 
 
 
