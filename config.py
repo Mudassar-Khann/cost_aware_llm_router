be592bfaa3a3aa1299976dd19ac886d0f1d37916
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-16 19:02:17 +0500

    feat: add user prompts and temperature settings to config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+class Config:
+
+    system_promt ={
+        "ver_concise" : "Act as expert Software Enginner give give effcient code and tell why it works ",
+        "concise" : "Give the concise and relevent answer to the problem",
+        "detail"  :  "Explain like subject expert give analogies and real life explan to uderstand the topic better "
+    }
+
+    temprature = {
+        "very_concise" : 0.1,
+        "concise" : 0.3,
+        "detail"  : 0.7
+    }
+
+
