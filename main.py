8d9f7fd99a42179741a3736c3ffa4b396b51b179
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-17 22:43:19 +0500

    feat: Add send_response function to handle API requests and errors in llm_client.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from router import ModelSelection
+import llm_client
 
-input("> Enter your 'query' for model response or type 'exist' to leave the sesstion: ")
-while True:
-    query = input(">")
+ms = ModelSelection()
 
-    if query == "exist":
-        print("Ending sesstion..")
-        break
+print("> Enter your 'query' for model response or type 'exist' to leave the sesstion: ")
+# while True:
+query = input(">").strip()
+
+if query == "exist":
+    print("Ending sesstion..")
+    # break
+user_query = ms.selct_model(query)
+response = llm_client.send_response(user_query)
+
+print(f"Model : {response}")
+
 
 
 
