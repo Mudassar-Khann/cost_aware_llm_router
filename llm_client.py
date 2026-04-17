5b2133a543bb98d056c698c88ac8ef8155c4c483
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-17 22:42:25 +0500

    feat: add clean code to llm_client.py and add error handling for api response

--- llm_client.py
+++ llm_client.py
@@ -2,33 +2,37 @@ from dotenv import load_dotenv
 import os
 import requests
 
-load_dotenv()
-
-openai_key = os.getenv("OPENAI_API_KEY")
-
-response = requests.post(
-  url="https://openrouter.ai/api/v1/chat/completions",
-  headers={
-    "Authorization": f"Bearer {openai_key}",
-    "Content-Type": "application/json",
-  },
-  json={
-    "model": "openai/gpt-oss-120b:free",
-    "messages": [
-        {
-          "role": "user",
-          "content": "How many r's are in the word 'strawberry'?"
-        }
-      ],
 
 
-  }
-)
-
-# Extract the assistant message with reasoning_details
-response = response.json()
-response = response['choices'][0]['message']["content"]
-
-print(response)
 
 
+errors = {
+    400 : "Api request is invalid somthing is missing fix your input",
+    401 : "unauthorized tookes missing or wrong api",
+    404 : "Model you requsted dosn't exist ",
+    429 : "limit exceeded",
+    500 : "sever error"
+}
+
+
+def send_response(data: dict, errors = errors):
+
+    if not data:
+        return "can't give answer to empty message"
+
+    load_dotenv()
+    openai_api = os.getenv("OPENAI_API_KEY")
+
+    url = "https://openrouter.ai/api/v1/chat/completions"
+
+    header = {
+        "Autharization" : f"Bearer {openai_api}",
+        "Content_Type" : "application/json"
+    }
+
+    response = requests.post(url, headers=header, json=data)
+
+    if response.status_code in errors:
+        return errors.get(response.status_code)
+
+    return response["choices"][0]["message"]["content"]
