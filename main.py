510d9d03b1e60717271e89cbf6366a958dea031c
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-20 19:32:58 +0500

    feat: did some chnages in main

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from router import ModelSelection
 import llm_client
+import time
 
 ms = ModelSelection()
 
+
 print("> Enter your 'query' for model response or type 'exist' to leave the sesstion: ")
-# while True:
-query = input(">").strip()
+while True:
+    query = input(">").strip()
+    star_time = time.perf_counter()
 
-if query == "exist":
-    print("Ending sesstion..")
-    # break
-user_query = ms.selct_model(query)
-response = llm_client.send_response(user_query)
+    if query == "exit":
+        print("Ending sesstion..")
+        break
+    user_query = ms.selct_model(query)
+    response = llm_client.send_response(user_query)
+    end_time = time.perf_counter()
 
-print(f"Model : {response}")
+    if "takin too long" in response or "unavlible" in response:
+        print("Server is busy retry after some time ")
+        break
+
+    print(f"Model : {response}")
 
 
 
