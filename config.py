c51513c9679e3c833413fee1af0c29a002ed8197
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-21 10:17:29 +0500

    fix: Update system prompt keys naming to match the function in router.py and temperature values

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 class Config:
 
     system_promt ={
-        "ver_concise" : "Act as expert Software Enginner give give effcient code and tell why it works ",
-        "concise" : "Give the concise and relevent answer to the problem",
-        "detail"  :  "Explain like subject expert give analogies and real life explan to uderstand the topic better "
+        "code" : "Act as expert Software Enginner give effcient code and tell why it works",
+        "small" : "Give the concise and relevent answer to the problem",
+        "detail"  :  "Explain like subject expert give analogies and real life explan to uderstand the topic better"
     }
 
     temprature = {
-        "very_concise" : 0.1,
-        "concise" : 0.3,
+        "code" : 0.2,
+        "small" : 0.3,
         "detail"  : 0.7
     }
 
