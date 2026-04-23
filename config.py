fb024c1ef9b7ca5322dcd662acdc9383e8183dfb
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-23 22:26:19 +0500

    feat: add settings class to manage configuration and environment variables

--- config.py
+++ config.py
@@ -1,8 +1,21 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    openai_api_key: str = "API key missing"
+
+
+
 class Config:
+
+    env_file = ".env"
+
+
 
     system_promt ={
         "code" : "Act as expert Software Enginner give effcient code and tell why it works",
-        "small" : "Give the concise and relevent answer to the problem",
+        "small" : "Give the concise and relevent answer to the qustion make sure not to add extra details",
         "detail"  :  "Explain like subject expert give analogies and real life explan to uderstand the topic better"
     }
 
@@ -10,6 +23,10 @@ class Config:
         "code" : 0.2,
         "small" : 0.3,
         "detail"  : 0.7
+    }
+
+    cost_of_models = {
+        "cheap" :
     }
 
 
