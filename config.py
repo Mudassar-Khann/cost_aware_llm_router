2d4cfcaa0f42227e2e78f7d7a7fa46946c640c20
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-26 12:48:34 +0500

    refactored: config.py to remove pydantic and simplify configuration management

--- config.py
+++ config.py
@@ -1,43 +1,18 @@
-from pydantic_settings import BaseSettings
-
-
-class Settings(BaseSettings):
-
-    openai_api_key: str = "API key missing"
-
-
-
 class Config:
+    CHEAP_MODEL = "openai/gpt-oss-20b:free"
+    EXPENSIVE_MODEL = "openai/gpt-oss-120b:free"
 
-    env_file = ".env"
+    SYSTEM_PROMPT = "You are a helpful assistant."
 
+    TEMPERATURE = 0.5
 
-
-    system_promt ={
-        "code" : "Act as expert Software Enginner give effcient code and tell why it works",
-        "small" : "Give the concise and relevent answer to the qustion make sure not to add extra details",
-        "detail"  :  "Explain like subject expert give analogies and real life explan to uderstand the topic better"
+    PRICING = {
+        CHEAP_MODEL: {
+            "input": 3,
+            "output": 15
+        },
+        EXPENSIVE_MODEL: {
+            "input": 5,
+            "output": 25
+        }
     }
-
-    temprature = {
-        "code" : 0.2,
-        "small" : 0.3,
-        "detail"  : 0.7
-    }
-
-    Models_Token_Pricing  = {
-
-    "openai/gpt-oss-20b:free" : {
-        "cost_per_1m_input" : 3,
-        "cost_per_1m_output" : 15
-    },
-    "openai/gpt-oss-120b:free" : {
-        "cost_per_1m_input" : 5,
-        "cost_per_1m_output" : 25
-    }
-
-
-    }
-
-    
-
