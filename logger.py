3c10430ab54a4b503559129fe1ecc467a7e463d9
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-04-27 21:43:48 +0500

    feat:: implement get_logger function to create a logger instance with file handler and formatter.

--- logger.py
+++ logger.py
@@ -1,13 +1,18 @@
 import logging
 from pathlib import Path
 
+def get_logger():
+    Path("logs").mkdir(exist_ok=True)
 
-Path("logs").mkdir(exist_ok=True)
+    logger = logging.getLogger("app")
+    logger.setLevel(logging.INFO)
 
-logging.basicConfig(
-    filename="logs/app.log",
-    level=logging.INFO,
-    filemode="a",
-    encoding="utf-8",
-    format="%(asctime)s - [%(levelname)s] - [%(name)s]- %(message)s"
-)
+    if not logger.handlers:
+        handler = logging.FileHandler("logs/app.log", encoding="utf-8")
+        formatter = logging.Formatter(
+            "%(asctime)s | %(levelname)s | %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    return logger
