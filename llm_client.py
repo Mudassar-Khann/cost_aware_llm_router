8e9894c46d91dfdb407940dcebe17216f5c4ae08
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-05-22 23:43:13 +0500

    Feat: reafactored llm_client to use openai python library instead of requests

--- llm_client.py
+++ llm_client.py
@@ -1,6 +1,4 @@
-import requests
 from config import Settings
-import os
 from openai import OpenAI
 
 settings = Settings()
@@ -8,34 +6,25 @@ settings = Settings()
 
 class LLMClient:
     def __init__(self):
-        self.api_key = settings.openrouter_api_key
-        self.url = "https://openrouter.ai/api/v1/chat/completions"
+
+        self.Client = OpenAI(
+            api_key = settings.openrouter_api_key,
+            base_url= "https://openrouter.ai/api/v1"
+        )
+
 
     def send(self, payload):
 
-
         try:
-            response = OpenAI.completions(
-                self.url,
-                headers={
-                    "Authorization": f"Bearer {self.api_key}",
-                    "Content-Type": "application/json"
-                },
-                json=payload,
+            response = self.Client.chat.completions.create(
+                messages= payload,
                 timeout=15
+
             )
-
-
-            if not response.ok:
-                return {
-                    "success": False,
-                    "data": None,
-                    "error": f"HTTP {response.status_code}: {response.text}"
-                }
 
             return {
                 "success": True,
-                "data": response.json(),
+                "data": response,
                 "error": None
             }
 
