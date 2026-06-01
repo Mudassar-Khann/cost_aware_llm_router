cf66759358e25a05a97cae5d4193a9cbceeaa621
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-06-01 20:38:27 +0500

    Add: Error classes for LLM exceptions in errors.py

---
+++ errors.py
@@ -0,0 +1,43 @@
+class LLMError(Exception):
+    def __init__(
+            self,
+            message: str,
+            provider: str = None,
+            model: str = None,
+            raw_response: str = None
+            ):
+
+        super.__init__(message)
+        self.provider = provider
+        self.model = model
+        self.raw_response = raw_response
+
+
+class AuthenticationError(LLMError):
+    pass
+
+
+class RateLimitError(LLMError):
+    pass
+
+
+class InvalidRequestError(LLMError):
+    pass
+
+
+class ModelNotFoundError(LLMError):
+    pass
+
+
+class APIConnectionError(LLMError):
+    pass
+
+
+class ProviderError(LLMError):
+    pass
+
+class TimeOutError(LLMError):
+    pass
+
+class NetworkError(LLMError):
+    pass
