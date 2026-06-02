4978742e067697beec8e9e3d0c0c775b68975f9e
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-06-02 12:25:32 +0500

    Add: Comprehensive error handling for LLMClient.send() method

--- llm_client.py
+++ llm_client.py
@@ -1,5 +1,7 @@
 from config import Settings
 from openai import OpenAI
+from errors import (LLMError, RateLimitError, InvalidRequestError,
+ NetworkError, TimeOutError, AuthenticationError, ModelNotFoundError, ProviderError)
 
 settings = Settings()
 
@@ -12,7 +14,7 @@ class LLMClient:
             base_url= "https://openrouter.ai/api/v1"
         )
 
-    
+
     def send(self, payload):
 
         try:
@@ -31,9 +33,52 @@ class LLMClient:
                 "error": None
             }
 
-        except Exception as e:
+        except AuthenticationError:
             return {
                 "success": False,
                 "data": None,
-                "error": str(e)
+                "error": AuthenticationError("Write APi key correctly")
+            }
+
+        except InvalidRequestError:
+            return {
+                "success": False,
+                "data": None,
+                "error": InvalidRequestError("Invalid Request")
+            }
+        except RateLimitError:
+            return {
+                "success": False,
+                "data": None,
+                "error": RateLimitError("Token Limit Exceeded")
+            }
+        except ProviderError:
+            return {
+                "success": False,
+                "data": None,
+                "error": ProviderError("Server Side error")
+            }
+        except ModelNotFoundError:
+            return {
+                "success": False,
+                "data": None,
+                "error": ModelNotFoundError("This model dosn't exist")
+            }
+        except TimeOutError:
+            return {
+                "success": False,
+                "data": None,
+                "error": TimeOutError("Given Time Exceded")
+            }
+        except NetworkError:
+            return {
+                "success": False,
+                "data": None,
+                "error": NetworkError("Network Busy")
+            }
+        except LLMError:
+            return {
+                "success": False,
+                "data": None,
+                "error": LLMError
             }
