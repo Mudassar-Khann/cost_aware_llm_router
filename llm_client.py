075c95064dea737ead290e0e6dace38c61fc8ce2
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-06-02 22:57:46 +0500

    feat:Add provider name and model to error responses in LLMClient

--- llm_client.py
+++ llm_client.py
@@ -9,6 +9,8 @@ settings = Settings()
 class LLMClient:
     def __init__(self):
 
+        self.provider_name = "OpenRouter"
+
         self.Client = OpenAI(
             api_key = settings.openrouter_api_key,
             base_url= "https://openrouter.ai/api/v1"
@@ -17,9 +19,12 @@ class LLMClient:
 
     def send(self, payload):
 
+        model = payload["model"]
+
+
         try:
             response = self.Client.chat.completions.create(
-                model= payload["model"],
+                model= model,
                 messages= payload["messages"],
                 temperature= payload["temperature"],
 
@@ -37,55 +42,55 @@ class LLMClient:
             return {
                 "success": False,
                 "data": None,
-                "error": AuthenticationError("Write APi key correctly", model= payload["model"])
+                "error": AuthenticationError("Write APi key correctly", model= model, provider=self.provider_name)
             }
 
         except openai.PermissionDeniedError:
             return {
                 "success": False,
                 "data": None,
-                "error": PermissionDeniedError("You Do Not have the permission to acess this material", model= payload["model"])
+                "error": PermissionDeniedError("You Do Not have the permission to acess this material", model= model, provider=self.provider_name)
             }
 
         except openai.BadRequestError:
             return {
                 "success": False,
                 "data": None,
-                "error": InvalidRequestError("Invalid Request")
+                "error": InvalidRequestError("Invalid Request", model= model, provider=self.provider_name)
             }
         except openai.RateLimitError:
             return {
                 "success": False,
                 "data": None,
-                "error": RateLimitError("Token Limit Exceeded")
+                "error": RateLimitError("Token Limit Exceeded", model= model, provider=self.provider_name)
             }
         except openai.InternalServerError:
             return {
                 "success": False,
                 "data": None,
-                "error": ProviderError("Server Side error", provider= "OpenRouter")
+                "error": ProviderError("Server Side error", model= model, provider=self.provider_name)
             }
         except openai.NotFoundError:
             return {
                 "success": False,
                 "data": None,
-                "error": ModelNotFoundError("This model dosn't exist", model= payload["model"])
+                "error": ModelNotFoundError("This model dosn't exist", model= model, provider=self.provider_name)
             }
         except openai.APITimeoutError:
             return {
                 "success": False,
                 "data": None,
-                "error": TimeoutError("Given Time Exceded")
+                "error": TimeoutError("Given Time Exceded", model= model, provider=self.provider_name)
             }
         except openai.APIConnectionError:
             return {
                 "success": False,
                 "data": None,
-                "error": NetworkError("Network Busy")
+                "error": NetworkError("Network Busy", model= model, provider=self.provider_name)
             }
         except Exception as e:
             return {
                 "success": False,
                 "data": None,
-                "error": ProviderError("Something went wrong", raw_response= str(e))
+                "error": ProviderError("Something went wrong", model= model, provider=self.provider_name, raw_response= str(e))
             }
