85d4b0a24f75539e09a192fa6006c51f6aea5567
Mudassar Khan <m.mudassaarkhan@gmail.com>
2026-06-03 16:46:11 +0500

    refactor: ADD AVAILABLE_MODELS  dict which contains all the details of the models, including pricing, display name, provider, and availability. This allows for easier management of models and their attributes in one place. and max retries for API calls

--- config.py
+++ config.py
@@ -6,20 +6,36 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
 class Config:
-    CHEAP_MODEL = "openai/gpt-oss-20b:free"
-    EXPENSIVE_MODEL = "openai/gpt-oss-120b:free"
 
     SYSTEM_PROMPT = "You are a helpful assistant."
 
     TEMPERATURE = 0.5
 
-    PRICING = {
-        CHEAP_MODEL: {
+    MAX_RETRIES = 3
+
+
+    AVAILABLE_MODELS = {
+    "gpt_oss_20b": {
+        "api_name": "openai/gpt-oss-20b:free",
+        "display_name": "GPT OSS 20B",
+        "provider": "OpenRouter",
+        "company": "OpenAI",
+        "available": True,
+        "pricing": {
             "input": 3,
             "output": 15
-        },
-        EXPENSIVE_MODEL: {
+        }
+    },
+
+    "gpt_oss_120b": {
+        "api_name": "openai/gpt-oss-120b:free",
+        "display_name": "GPT OSS 120B",
+        "provider": "OpenRouter",
+        "company": "OpenAI",
+        "available": True,
+        "pricing": {
             "input": 5,
             "output": 25
         }
     }
+}
